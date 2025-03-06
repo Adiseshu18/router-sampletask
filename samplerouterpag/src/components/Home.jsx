1c9f8f9621df0a6fee04fe21b80639c017c4f30e
Adiseshu18 <adiseshu503@gmail.com>
2025-03-06 10:02:49 +0530

    modified home

--- samplerouterpag/src/components/Home.jsx
+++ samplerouterpag/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Button, CircularProgress } from '@mui/material';
+import { Container, Typography, Button, CircularProgress, Box } from '@mui/material';
 
 function Home() {
   const [count, setCount] = useState(0);
@@ -27,9 +27,32 @@ function Home() {
       ) : (
         <Typography variant="h6">Count: {count}</Typography>
       )}
-      <Button variant="contained" onClick={increment}>
+      <Button variant="contained" onClick={increment} sx={{ marginBottom: 2 }}>
         Increment
       </Button>
+      
+      <Box sx={{ marginTop: 4 }}>
+        <Typography variant="h6" gutterBottom>
+          Follow us on Social Media:
+        </Typography>
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          href="https://www.youtube.com" 
+          target="_blank" 
+          sx={{ marginRight: 2 }}
+        >
+          YouTube
+        </Button>
+        <Button 
+          variant="outlined" 
+          color="secondary" 
+          href="https://www.instagram.com" 
+          target="_blank"
+        >
+          Instagram
+        </Button>
+      </Box>
     </Container>
   );
 }
